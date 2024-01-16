2fa46c736ddb45a2f52b3fff21bd03f4f2f90848
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-16 10:33:34 +0900

    Add Post model

---
+++ model/post.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type (
+	Post struct {
+		ID        string    `json:"id"`
+		Title     string    `json:"title"`
+		Content   string    `json:"content"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+)
